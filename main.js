3b27322d56bf204ddd6c54773291e821f55cd2dc
Ahmed Fathi <98648216+ahmedfathy112@users.noreply.github.com>
2024-03-19 22:17:33 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -2,11 +2,13 @@ var passwordValue = document.getElementById("password");
 var ConfirmPass = document.getElementById("ConfirmPassword");
 var btn = document.getElementById("btnSubmit");
 var result = document.getElementById("result");
-
+var length = document.getElementById("length");
 btn.addEventListener("click", (e) => {
+  let value = passwordValue.value;
   e.preventDefault();
   if (passwordValue.value === ConfirmPass.value) {
     result.innerHTML = "THE RESULT IS TRUE";
+    length.innerHTML = `THE LENGTH IS: ${value.length}`;
   } else {
     result.innerHTML = "THE RESULT IS FALSE";
   }
